85b50f81b58fc92c4f6b189d27081b729a74e38c
binaspace <binaspaceprotocol@gmail.com>
2021-10-17 20:34:13 -0700

    Updates

---
+++ precache-manifest.bd234f1f942e9809ebbbb4eb0c787508.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd50f6341f9b0feb5035200a0f89706",
+    "url": "/opensea-whitelabel/index.html"
+  },
+  {
+    "revision": "2ab0208c3569e9b65e3d",
+    "url": "/opensea-whitelabel/static/css/main.a1eb92ab.chunk.css"
+  },
+  {
+    "revision": "aa24f0481cc918f6fbef",
+    "url": "/opensea-whitelabel/static/js/2.21c8c7b1.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "/opensea-whitelabel/static/js/2.21c8c7b1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2ab0208c3569e9b65e3d",
+    "url": "/opensea-whitelabel/static/js/main.72dda9ed.chunk.js"
+  },
+  {
+    "revision": "330ec4368c0ae6b37354",
+    "url": "/opensea-whitelabel/static/js/runtime-main.ce2a9a07.js"
+  },
+  {
+    "revision": "f85cb359a26d2046f998ed931cc0359f",
+    "url": "/opensea-whitelabel/static/media/hero.f85cb359.jpg"
+  }
+]);
